345105f62151b68ad9ffc768f6f4e68bf8179a99
vishnu <vbhandari9561@gmail.com>
2019-09-25 12:43:53 +0530

    aa

--- New folder/models.py
+++ New folder/models.py
@@ -13,9 +13,9 @@ class Student(models.Model):
       db_table = "student_info"
 
 
-s1 = Student(name='A',mail='gmail1',address='Pune1',phonenumber=1234)
-s2 = Student(name='B',mail='gmail2',address='Pune2',phonenumber=5678)
-s3 = Student(name='C',mail='gmail3',address='Pune3',phonenumber=1111)
-s4 = Student(name='D',mail='gmail4',address='Pune4',phonenumber=2222)
-s5 = Student(name='E',mail='gmail5',address='Pune5',phonenumber=3333)
+# s1 = Student(name='A',mail='gmail1',address='Pune1',phonenumber=1234)
+# s2 = Student(name='B',mail='gmail2',address='Pune2',phonenumber=5678)
+# s3 = Student(name='C',mail='gmail3',address='Pune3',phonenumber=1111)
+# s4 = Student(name='D',mail='gmail4',address='Pune4',phonenumber=2222)
+# s5 = Student(name='E',mail='gmail5',address='Pune5',phonenumber=3333)
 
